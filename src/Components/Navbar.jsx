292f62cea100a2a90df7136f57d7261ecc2bf51b
Akash Srivastava <akashpsit13@gmail.com>
2024-12-21 22:05:14 +0530

    sample

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 import "../App.css";
 
-export function Navbar({ user, handleLogout }) {
-  return (
-    <div className="containerNav">
-         <img className="img" src="https://as1.ftcdn.net/v2/jpg/09/57/85/46/1000_F_957854646_zvLI7pkE2rsQGZM3VlCRgoLEp3bU1yWI.jpg" alt="" />
-        <Link to={"/"}>Home</Link>
-        <Link to={"/add"}>Add</Link>
-        <Link to={"/products"}>Product</Link>
+// export function Navbar({ user, handleLogout }) {
+//   return (
+//     <div className="containerNav">
+//          <img className="img" src="https://as1.ftcdn.net/v2/jpg/09/57/85/46/1000_F_957854646_zvLI7pkE2rsQGZM3VlCRgoLEp3bU1yWI.jpg" alt="" />
+//         <Link to={"/"}>Home</Link>
+//         <Link to={"/add"}>Add</Link>
+//         <Link to={"/products"}>Product</Link>
        
-        <input className="inputsearch" type="text" placeholder="Search " />
-        <img src="https://img.icons8.com/?size=100&id=72704&format=png&color=000000" alt="" />
-        <Link to={"/login"}>Login</Link>
+//         <input className="inputsearch" type="text" placeholder="Search " />
+//         <img src="https://img.icons8.com/?size=100&id=72704&format=png&color=000000" alt="" />
+//         <Link to={"/login"}>Login</Link>
         <Link to={"/about"}>About Us</Link>
 
 
