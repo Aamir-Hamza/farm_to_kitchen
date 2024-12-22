21a6537cbf4687c8363846eed324f95dcb633faf
strivendra002 <110083471+strivendra002@users.noreply.github.com>
2024-12-22 17:05:31 +0530

    Update index.js

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from './cart';
+import cartReducer from "./cart";
 
 export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        // user: ...
-    }
-})
+  reducer: {
+    cart: cartReducer,
+    // Add other reducers here as needed.
+  },
+});
